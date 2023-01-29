cff34b8a4b94ce56938b24ddefbda6d362117cb8
jesm17 <sanchez17082003@gmail.com>
2023-01-29 17:22:33 -0500

    produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   base_url_back: 'https://prueba-back.herokuapp.com/api',
   base_url_pokeApi: 'https://pokeapi.co/api/v2/',
 };
